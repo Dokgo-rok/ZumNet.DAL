29e538913c2abd4c5541980a2c34ca343027ef9a
neostyx <neostyx@gmail.com>
2021-04-19 17:05:35 +0900

    소스 위치 변경

---
+++ ServiceDac/Src/EvaluationDac.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ZumNet.DAL.Base;
+
+namespace ServiceDac
+{
+	public class EvaluationDac : DacBase
+	{
+		
+	}
+}
