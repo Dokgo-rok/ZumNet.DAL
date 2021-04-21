d71d47e0ef2e13f13422ba7fffb8390a96884806
neostyx <neostyx@gmail.com>
2021-04-21 17:24:05 +0900

    Framework 변경에 따른 Dac 변경

--- ServiceDac/Src/DomainDac.cs
+++ ServiceDac/Src/DomainDac.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -7,7 +6,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using ZumNet.DAL.Base;
+using ZumNet.Framework.Base;
+using ZumNet.Framework.Data;
 
 namespace ZumNet.DAL.ServiceDac
 {
--- ServiceDac/Src/ResourceDac.cs
+++ ServiceDac/Src/ResourceDac.cs
@@ -6,7 +6,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using ZumNet.DAL.Base;
+using ZumNet.Framework.Base;
+using ZumNet.Framework.Data;
 
 namespace ZumNet.DAL.ServiceDac
 {
