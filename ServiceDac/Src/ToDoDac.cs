6c309851e10f659f8e9b10053b522ff6b900a189
BYRL <byeongrok.lee@gmail.com>
2021-06-04 16:02:59 +0900

    WorkTimeDac, ToDoDac 추가

---
+++ ServiceDac/Src/ToDoDac.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using ZumNet.Framework.Base;
+using ZumNet.Framework.Data;
+
+namespace ZumNet.DAL.ServiceDac
+{
+    public class ToDoDac : DacBase
+    {
+		/// <summary>
+		/// 
+		/// </summary>
+		public ToDoDac(string connectionString = "") : base(connectionString)
+		{
+
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public ToDoDac(SqlConnection connection) : base(connection)
+		{
+
+		}
+	}
+}
---
+++ ServiceDac/Src/WorkTimeDac.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using ZumNet.Framework.Base;
+using ZumNet.Framework.Data;
+
+namespace ZumNet.DAL.ServiceDac
+{
+    /// <summary>
+	/// 
+	/// </summary>
+	public class WorkTimeDac : DacBase
+    {
+		/// <summary>
+		/// 
+		/// </summary>
+		public WorkTimeDac(string connectionString = "") : base(connectionString)
+		{
+
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public WorkTimeDac(SqlConnection connection) : base(connection)
+		{
+
+		}
+
+
+	}
+}
