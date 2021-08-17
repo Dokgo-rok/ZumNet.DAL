28dec5badab0a66e80e628eddc2970dbc44b93d8
BYRL <byeongrok.lee@gmail.com>
2021-08-17 15:54:55 +0900

    결재, 인터페이스 프로젝트 추가

---
+++ FlowDac/Src/ProcessDac.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ZumNet.Framework.Base;
+using ZumNet.Framework.Data;
+
+namespace ZumNet.DAL.FlowDac
+{
+    public class ProcessDac : DacBase
+    {
+		/// <summary>
+		/// 
+		/// </summary>
+		public ProcessDac(string connectionString = "") : base(connectionString)
+		{
+
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public ProcessDac(SqlConnection connection) : base(connection)
+		{
+
+		}
+	}
+}
